eba3099b6e39c32fb76a7affa90a1cc5f4df57f4
Ethank-lcwc <144791953+Ethank-lcwc@users.noreply.github.com>
2024-09-16 13:10:10 +0000

    hiiiiii

--- task.py
+++ task.py
@@ -1,5 +1,9 @@
 def task():
 
     # grade should be a tuple containing student name (str), subject (str) and score (int)
+    name = input("student name \n")
+    sub = input("subject studied \n")
+    score = int(input("test score \n"))
+    grade = (name , sub , score)
 
     return grade
